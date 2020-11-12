a2f6fea856b9dea0bbacb52569e9172ce3b28b4d
Minh Nhut Huynh <Ne7ven@gmail.com>
2020-11-12 21:07:47 +1100

    add node-fetch api and test api

--- src/controllers/CustomerController.ts
+++ src/controllers/CustomerController.ts
@@ -1,10 +1,35 @@
-import { JsonController, Body, Post, Get } from 'routing-controllers';
+import { JsonController, Get, Post, Body } from 'routing-controllers';
+import fetch from 'node-fetch';
 
 @JsonController('/customer')
-export default class CheckYourCustomerController {
+export default class CustomerController {
 
     @Get()
     welcome(): string {
         return 'welcome';
+    }
+
+    @Post('/licenses')
+    async checkingLicenses(@Body() customer: any):Promise<any> {
+        const testData = { 
+            "birthDate" : "1985-02-08", 
+            "givenName" : "James", 
+            "middleName" : "Robert", 
+            "familyName" : "Smith", 
+            "licenceNumber" : "94977000", 
+            "stateOfIssue" : "NSW", 
+            "expiryDate" : "2020-01-01" 
+        };
+        const response = await fetch('https://australia-southeast1-reporting-290bc.cloudfunctions.net/driverlicence', {
+            headers: {
+                'Authorization': 'Bearer 03aa7ba718da920e0ea362c876505c6df32197940669c5b150711b03650a78cf',
+                'Content-Type': 'application/json'
+            },
+            method: 'POST',
+            body: JSON.stringify(testData)
+        });
+        const result = await response.json();
+        console.log('result', result);
+        return result;
     }
 }
